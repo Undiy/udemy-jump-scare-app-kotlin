8511eaab5589d98ff9bb639277403d086eb321e0
Undiy <undiy@yandex.ru>
2023-07-26 12:11:43 +0500

    SurpriseActivity setup

--- CounterShock/app/src/main/java/com/example/countershock/SurpriseActivity.kt
+++ CounterShock/app/src/main/java/com/example/countershock/SurpriseActivity.kt
@@ -1,13 +1,18 @@
 package com.example.countershock
 
 import android.content.ContentResolver
+import android.media.AudioAttributes
+import android.media.AudioManager
 import android.media.MediaPlayer
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
+import android.view.MotionEvent
 import android.view.View
+import android.view.WindowManager
 import android.widget.ImageView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import java.io.File
 
@@ -20,22 +25,30 @@ class SurpriseActivity : AppCompatActivity() {
     lateinit var tts: TextToSpeech
     lateinit var mediaPlayer: MediaPlayer
 
+    private var acceptTouches = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_surprise)
 
         imageView = findViewById(R.id.imageView)
 
-        photoUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE
-                + File.pathSeparator + File.separator + File.separator
-                + packageName + "/drawable/lama")
+        photoUri = Uri.parse(
+            ContentResolver.SCHEME_ANDROID_RESOURCE
+                    + File.pathSeparator + File.separator + File.separator
+                    + packageName + "/drawable/lama"
+        )
 
-        soundUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE
-                + File.pathSeparator + File.separator + File.separator
-                + packageName + "/raw/behind_you")
+        soundUri = Uri.parse(
+            ContentResolver.SCHEME_ANDROID_RESOURCE
+                    + File.pathSeparator + File.separator + File.separator
+                    + packageName + "/raw/see_you"
+        )
 
-        showImage()
-        playSoundClip()
+        Toast.makeText(this, "Ready", Toast.LENGTH_SHORT).show()
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
     }
 
     private fun showImage() {
@@ -48,6 +61,22 @@ class SurpriseActivity : AppCompatActivity() {
 
     private fun playSoundClip() {
         mediaPlayer = MediaPlayer.create(this, soundUri)
+        mediaPlayer.setOnCompletionListener {
+            finish()
+        }
         mediaPlayer.start()
+    }
+
+    private fun userProvidedAction() {
+        if (acceptTouches) {
+            showImage()
+            playSoundClip()
+            acceptTouches = false
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        userProvidedAction()
+        return super.onTouchEvent(event)
     }
 }
